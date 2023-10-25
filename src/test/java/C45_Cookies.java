2bb7155e5e6c12adf840bf9dcc449059726d612c
ali <alig90901@gmail.com>
2023-10-25 21:31:51 +0100

    selenium

--- src/test/java/C45_Cookies.java
+++ src/test/java/C45_Cookies.java
@@ -1,2 +1,19 @@
+import org.junit.Test;
+
 public class C45_Cookies {
+    //***Cookie cookie  = driver.manage().cookie-methods
+
+    //Go to URL: https://kitchen.applitools.com/ingredients/cookie
+    //Get Cookie.
+    //Find the total number of cookies.
+    //Print all the cookies.
+    //Add Cookie.
+    //Edit Cookie.
+    //Delete Cookie.
+    //Delete All Cookies.
+    @Test
+    public void test1() {
+
+    }
 }
+
